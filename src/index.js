df82cc77883e7e053d10a6444fd5067ddc78710c
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-10-08 14:59:06 +0530

    Setting Up Socket IO

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
+const PORT = process.env.PORT || 3000;
 const publicDirPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirPath));
-
-const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send(`Hello Chat`)
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is up on Port : ${PORT}`);
 });
